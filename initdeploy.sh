e1ca31526140fd91e59026a83f41dcaf63ea1e9c
chechkysh <dentag240502@mail.ru>
2023-02-09 18:14:48 +0700

    front and more

---
+++ initdeploy.sh
@@ -0,0 +1,4 @@
+docker cp /www/server/panel/vhost/cert/looparadise.ru/fullchain.pem hotelparadise_back_dj_web_1:/etc/nginx/fullchain.pem
+docker cp /www/server/panel/vhost/cert/looparadise.ru/privkey.pem hotelparadise_back_dj_web_1:/etc/nginx/privkey.pem
+docker exec hotelparadise_back_dj_web_1 nginx -s reload
+docker cp hotelfront:/usr/src/app /www/wwwroot/looparadise.ru/
